5905493f201842e79fab0cbc8abb984de47bb776
emirhan <emirhan.battalbas@mobillium.com>
2021-02-12 10:23:57 +0300

    added support codable

--- MobilliumUserDefaults/Classes/Key.swift
+++ MobilliumUserDefaults/Classes/Key.swift
@@ -7,19 +7,7 @@
 
 import Foundation
 
-public class Key<ValueType>: DefaultsKey {
-    
-    public var value: ValueType? {
-        set {
-            /// Saves a 'value' using 'key'
-            Defaults.set(newValue, forKey: key)
-            Defaults.synchronize()
-        }
-        get {
-            /// Creates a 'value' using 'key'
-            return Defaults.object(forKey: key) as? ValueType
-        }
-    }
+public class Key<ValueType: Codable>: DefaultsKey {
     
     let key: String!
     
@@ -28,14 +16,96 @@ public class Key<ValueType>: DefaultsKey {
     }
     
     /// Removes a value using 'key'
-    public func remove() {
-        Defaults.removeObject(forKey: key)
+    public func remove(_ key: Key<ValueType>) {
+        Defaults.set(nil, forKey: key.key)
         Defaults.synchronize()
     }
     
     /// Returns `true` if `key` exists
-    public var has: Bool {
-        return Defaults.object(forKey: key) != nil
+    public func has(_ key: Key<ValueType>) -> Bool {
+        return Defaults.value(forKey: key.key) != nil
+    }
+    
+    /// - Returns: A `ValueType` or nil if the key was not found.
+    public func get<ValueType>(for key: Key<ValueType>) -> ValueType? {
+        if isSwiftCodableType(ValueType.self) || isFoundationCodableType(ValueType.self) {
+            return Defaults.value(forKey: key.key) as? ValueType
+        }
+        
+        guard let data = Defaults.data(forKey: key.key) else {
+            return nil
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let decoded = try decoder.decode(ValueType.self, from: data)
+            return decoded
+        } catch {
+            #if DEBUG
+            print(error)
+            #endif
+        }
+        
+        return nil
+        
+    }
+    
+    /// Sets a value associated with the specified key.
+    ///
+    /// - Parameters:
+    ///   - some: The value to set.
+    ///   - key: The associated `Key<ValueType>`.
+    public func set<ValueType>(_ value: ValueType, for key: Key<ValueType>) {
+        if isSwiftCodableType(ValueType.self) || isFoundationCodableType(ValueType.self) {
+            Defaults.set(value, forKey: key.key)
+            return
+        }
+        
+        do {
+            let encoder = JSONEncoder()
+            let encoded = try encoder.encode(value)
+            Defaults.set(encoded, forKey: key.key)
+            Defaults.synchronize()
+        } catch {
+            #if DEBUG
+            print(error)
+            #endif
+        }
+    }
+    
+    /// Removes given bundle's persistent domain
+    ///
+    /// - Parameter type: Bundle.
+    public func removeAll(bundle : Bundle = Bundle.main) {
+        guard let name = bundle.bundleIdentifier else { return }
+        Defaults.removePersistentDomain(forName: name)
+    }
+    
+    /// Checks if the specified type is a Codable from the Swift standard library.
+    ///
+    /// - Parameter type: The type.
+    /// - Returns: A boolean value.
+    private func isSwiftCodableType<ValueType>(_ type: ValueType.Type) -> Bool {
+        switch type {
+        case is String.Type, is Bool.Type, is Int.Type, is Float.Type, is Double.Type:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    /// Checks if the specified type is a Codable, from the Swift's core libraries
+    /// Foundation framework.
+    ///
+    /// - Parameter type: The type.
+    /// - Returns: A boolean value.
+    private func isFoundationCodableType<ValueType>(_ type: ValueType.Type) -> Bool {
+        switch type {
+        case is Date.Type:
+            return true
+        default:
+            return false
+        }
     }
     
 }
